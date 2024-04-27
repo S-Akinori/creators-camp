cd25946c48ec5fbf4162e09dbf3707287b8ae5d0
akinori <aki10.working@gmail.com>
2024-04-27 17:38:38 +0900

    fix: create about, sponser page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,10 +7,18 @@ import Button from "../components/atoms/Button";
 import ReactMarkdown from 'react-markdown';
 import { tocText } from "@/contents/toc";
 
-export default function Login() {
+export default function AboutPage() {
     return (
         <Container>
             <h1><TextShadow className="text-xl">当サイトについて</TextShadow></h1>
+            <Image src="/images/logo.png" className="mx-auto" alt="about" width={545} height={123} />
+            <div className="mt-8">
+                <p>Re-creator’s Camp には<span className="font-bold underline">「素材を再活用する、生き返らせる＝Recreate」</span>という思いを込めております。</p>
+                <p>でも、単に素材が集まる場所だけではなく、素材の向こうにある<span className="font-bold underline">「創作者＝Creator」</span>の皆さま<span className="font-bold underline">が「集まる場、交流する場＝Camp」</span>であってほしい。</p>
+                <p>素材を共有して、コミュニケーションをとって<span className="font-bold underline">「Re: Re: Re: 」</span>を積み重ねてほしい。</p>
+                <p>そのような沢山の思いを込めて<span className="font-bold underline">「Re-creator’s Camp」</span>を立ち上げました。</p>
+                <p>ぜひ皆さまの思いのこもった素材を通じて、少しでも創作活動に彩を持たせられたら幸いです</p>
+            </div>
         </Container>
     );
 }
--- app/sponser/page.tsx
+++ app/sponser/page.tsx
@@ -7,10 +7,23 @@ import Button from "../components/atoms/Button";
 import ReactMarkdown from 'react-markdown';
 import { tocText } from "@/contents/toc";
 
-export default function Login() {
+export default function SponserPage() {
     return (
         <Container>
-            <h1><TextShadow className="text-xl">スポンサー</TextShadow></h1>
+            <div className="relative">
+                <Image src="/images/bg-fv.png" className="mx-auto mb-4" alt="about" width={1920} height={530} />
+                <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"><TextShadow className="text-2xl">スポンサーの皆様</TextShadow></h1>
+            </div>
+            <div className="flex justify-center mb-4">
+                <Image src="/images/bow2.png" sizes="(max-width: 768px) 30vw" className="" alt="about" width={300} height={300} />
+                <Image src="/images/bow.png" sizes="(max-width: 768px) 30vw" alt="about" width={200} height={300} />
+            </div>
+            <div>
+                <p>Re-Creator’s Campはクラウドファンディングを通じて、多くの支援者様にサポート頂くことで、スタートすることができました。重ねて感謝申し上げます。</p>
+                <p>紆余曲折があり、多大なご心配をおかけいたしましたが、皆様の温かい応援もあり、ようやくサービスを開始することができました。</p>
+                <p>重ねて御礼申し上げます。</p>
+
+            </div>
         </Container>
     );
 }
